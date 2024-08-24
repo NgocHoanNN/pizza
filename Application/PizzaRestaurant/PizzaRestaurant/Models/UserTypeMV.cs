501d915e451292251b0c8e2849b82f122d372404
Hoancter <hoannguyen2k1hhbg@gmail.com>
2024-08-24 10:08:21 +0700

    2nd commit

---
+++ Application/PizzaRestaurant/PizzaRestaurant/Models/UserTypeMV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PizzaRestaurant.Models
+{
+    public class UserTypeMV
+    {
+
+
+        [Display(Name = "#Unique No")]
+        public int UserTypeID { get; set; }
+        [Display(Name = "User Type")]
+        [Required(ErrorMessage = "Required*")]
+        public string UserType { get; set; }
+    }
+}
